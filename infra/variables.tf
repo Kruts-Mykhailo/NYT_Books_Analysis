935bd27047e45fae5d64fbfef37c18ef674b1b82
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2024-09-06 19:15:33 +0200

    Test infra postgres db deployment

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "name_prefix" {
+  default     = "postgresqlfs"
+  description = "Prefix of the resource name."
+}
+
+variable "location" {
+  default     = "Italy North"
+  description = "Location of the resource."
+}
